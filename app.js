68ee30d297da3df609c2b5f7a9ca8abfb76348fc
Stx31 <kh1327303@gmail.com>
2023-10-02 16:18:57 +0200

    falta hora

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Función para guardar datos en el almacenamiento local
 function guardarDatos() {
     var email = document.getElementById("emailInput").value;
     var name = document.getElementById("nameInput").value;
@@ -8,10 +7,9 @@ function guardarDatos() {
     localStorage.setItem("userName", name);
     localStorage.setItem("userMessage", message);
 
-    mostrarDatos();
+    mostrarDatos(); 
 }
 
-// Función para mostrar los datos almacenados
 function mostrarDatos() {
     var email = localStorage.getItem("userEmail") || "";
     var name = localStorage.getItem("userName") || "";
@@ -22,30 +20,12 @@ function mostrarDatos() {
     document.getElementById("savedMessage").textContent = message;
 }
 
-// Función para borrar datos del almacenamiento local
 function borrarDatos() {
     localStorage.removeItem("userEmail");
     localStorage.removeItem("userName");
     localStorage.removeItem("userMessage");
 
-    mostrarDatos();
+    mostrarDatos(); 
 }
 
-// Asegurarse de que los datos se muestren cuando la página se carga
-window.onload = mostrarDatos;
-
-// Obtener elementos relevantes para mostrar la hora
-const mostrarHoraBtn = document.getElementById('mostrarHoraBtn');
-const horaClicP = document.getElementById('horaClic');
-
-// Agregar un evento al botón para mostrar la hora
-mostrarHoraBtn.addEventListener('click', () => {
-    const fecha = new Date();
-    const horas = fecha.getHours();
-    const minutos = fecha.getMinutes();
-    const segundos = fecha.getSeconds();
-
-    const horaFormateada = `${horas}:${minutos}:${segundos}`;
-
-    horaClicP.textContent = `Hora en el clic: ${horaFormateada}`;
-});
+window.onload = mostrarDatos; 
