9e4e0d51f0d39b024d7bfa510853ca8dabdd5c53
Stx31 <kh1327303@gmail.com>
2023-10-15 14:32:45 +0200

    usuario admin 1 contra admin1

--- login.js
+++ login.js
@@ -1,38 +1,10 @@
-const loginForm = document.getElementById('loginForm');
-const showDataButton = document.querySelector('button[type="button"]');
-
-
-let userDataArray = [];
-
-
-loginForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-
-    const userData = { username, password };
-    userDataArray.push(userData);
-    localStorage.setItem('userDataArray', JSON.stringify(userDataArray));
-    loginForm.reset();
-    alert('Datos guardados');
-});
-
-showDataButton.addEventListener('click', function () {
-
-    const storedData = localStorage.getItem('userDataArray');
-
-    if (storedData) {
-
-        const userDataArray = JSON.parse(storedData);
-        let dataToShow = "Datos guardados:\n";
-        userDataArray.forEach((userData, index) => {
-            dataToShow += `Usuario ${index + 1}: ${userData.username}, Contraseña ${index + 1}: ${userData.password}\n`;
-        });
-        alert(dataToShow);
-    } else {
-        alert('No se encontraron datos almacenados ');
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username === "admin1" && password === "admin1") {  
+        window.location.href = "index.html";
+    } else { 
+        alert("Contraseña incorrecta");
     }
 });
