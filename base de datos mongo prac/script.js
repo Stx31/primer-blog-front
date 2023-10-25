7252efbbc588a412ea83b4bb7b13059e4c6b1601
Stx31 <kh1327303@gmail.com>
2023-10-25 15:47:36 +0200

    login register

--- base de datos mongo prac/script.js
+++ base de datos mongo prac/script.js
@@ -1,9 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./User'); // Importa el modelo de usuario
+const ejs = require('ejs'); // Agrega el motor de plantillas EJS
 
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("/data")
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("data").innerHTML = data;
-      });
-  });
-  
+const app = express();
+app.use(express.json());
+app.set('view engine', 'ejs'); 
+
+
+mongoose.connect('mongodb://localhost:27017/your-database', { useNewUrlParser: true });
+
+
+app.get('/register', (req, res) => {
+    res.render('register'); 
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+app.listen(3000, () => {
+    console.log('Servidor en ejecución en el puerto 3000');
+});
