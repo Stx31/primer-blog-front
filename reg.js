e230ca0284663d82624003c5ac1a753027895e05
Stx31 <kh1327303@gmail.com>
2023-11-09 13:59:30 +0100

    concet backend to front

--- reg.js
+++ reg.js
@@ -94,3 +94,12 @@ mensajesDiv.addEventListener("click", function (event) {
 });
 
 mostrarMensajes();
+
+document.getElementById('getDataBtn').addEventListener('click', () => {
+    fetch('http://localhost:3001/api/data')
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('data').innerText = data.message;
+      })
+      .catch(error => console.error(error));
+  });
