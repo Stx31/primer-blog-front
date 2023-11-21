24c29e7f8477a75a7670c2e1d5b8f4401f79170b
Stx31 <kh1327303@gmail.com>
2023-11-21 15:36:11 +0100

    usuaario envia señal postaman

--- login.js
+++ login.js
@@ -1,10 +1,47 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    if (username === "admin1" && password === "admin1") {  
-        window.location.href = "index.html";
-    } else { 
-        alert("Contraseña incorrecta");
+function submitForm() {
+    var usernameInput = document.getElementById('nombre').value;
+    var passwordInput = document.getElementById('edad').value;
+
+
+    if (usernameInput === '1234' && passwordInput === '1234') {
+    
+        window.location.href = 'index.html';
+    } else {
+       
+        console.log('Credenciales incorrectas');
     }
-});
+}
+function submitForm() {
+    var usernameInput = document.getElementById('nombre').value;
+    var passwordInput = document.getElementById('edad').value;
+
+  
+    var formData = new FormData();
+    formData.append('nombre', usernameInput);
+    formData.append('edad', passwordInput);
+
+   
+    var expressUrl = 'http://localhost:3000/guardar-datos';
+
+  
+    fetch(expressUrl, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Respuesta de Express:', data);
+        
+        
+        if (usernameInput === '1234' && passwordInput === '1234') {
+          
+            window.location.href = 'index.html';
+        } else {
+         
+            console.log('Credenciales incorrectas');
+        }
+    })
+    .catch(error => {
+        console.error('Error al realizar la solicitud:', error);
+    });
+}
--- server.js
+++ server.js
@@ -5,25 +5,27 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-const savedData = []; 
+const savedData = [];
 
 app.post('/guardar-datos', (req, res) => {
-    const { author, title, message } = req.body;
+    const { nombre, edad } = req.body;
 
-    savedData.push({ author, title, message });
-
-
-    const successMessage = 'Datos guardados con éxito. Datos actuales: ' + JSON.stringify(savedData);
-    res.status(201).send(successMessage);
+  
+    if (nombre === '1234' && edad === '1234') {
+        const successMessage = 'Credenciales correctas. Datos guardados con éxito.';
+        res.status(201).json({ message: successMessage });
+    } else {
+        const errorMessage = 'Credenciales incorrectas. No se guardaron los datos.';
+        res.status(401).json({ error: errorMessage });
+    }
 });
 
 app.get('/obtener-datos', (req, res) => {
     res.json(savedData);
 });
 
-
 app.delete('/borrar-datos', (req, res) => {
-    savedData.length = 0; 
+    savedData.length = 0;
     res.send('Datos borrados con éxito');
 });
 
