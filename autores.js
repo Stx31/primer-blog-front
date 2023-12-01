f412cbd5e754a76e1c0c0d6a739c68499f09c7f5
Stx31 <kh1327303@gmail.com>
2023-12-01 15:30:24 +0100

    autor boton , autor vista con postman

--- autores.js
+++ autores.js
@@ -3,19 +3,49 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function loadAuthors() {
-    fetch('http://localhost:3000/obtenerAutores')
+    fetch('http://localhost:3000/obtenerMensajes')
         .then(response => response.json())
         .then(data => {
-            const autoresContainer = document.getElementById('autores');
-            autoresContainer.innerHTML = '';
+            const authorsContainer = document.getElementById('authorsContainer');
+            authorsContainer.innerHTML = '<h2>Autores:</h2>';
 
-            data.autores.forEach(author => {
-                const authorDiv = document.createElement('div');
-                authorDiv.classList.add('author');
-                authorDiv.innerHTML = `<p>${author}</p>`;
+            const authorsSet = new Set();
 
-                autoresContainer.appendChild(authorDiv);
+            data.mensajes.forEach(({ author }) => {
+                authorsSet.add(author);
+            });
+
+            authorsSet.forEach(author => {
+                const authorBox = document.createElement('div');
+                authorBox.classList.add('author-box');
+
+                const authorItem = document.createElement('span');
+                authorItem.textContent = author;
+
+                const deleteButton = document.createElement('button');
+                deleteButton.textContent = 'Borrar';
+                deleteButton.addEventListener('click', function () {
+                    deleteAuthor(author);
+                    authorBox.remove();
+                });
+
+                authorBox.appendChild(authorItem);
+                authorBox.appendChild(deleteButton);
+                authorsContainer.appendChild(authorBox);
             });
         })
+        .catch(error => console.error('Error:', error));
+}
+
+function deleteAuthor(author) {
+    fetch('http://localhost:3000/borrarAutor', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ author }),
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
         .catch(error => console.error('Error:', error));
 }
