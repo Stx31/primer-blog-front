ba858eee417319fb04d1ab690e78b51fb8953fba
Stx31 <kh1327303@gmail.com>
2023-12-11 15:29:51 +0100

    login contraseña postman

--- login.js
+++ login.js
@@ -2,26 +2,29 @@ function authenticateAndRedirect() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    fetch('http://localhost:3000/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            if (data.success) {
-                redirectToIndex();
-            } else {
-                console.log('La autenticación falló');
-            }
-
-         
-            if (window.postman) {
-                window.postman.sendMessage(data);
-            }
+    if (password === '123') {
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
         })
-        .catch(error => console.error('Error:', error));
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.success) {
+                    redirectToIndex();
+                } else {
+                    console.log('La autenticación falló');
+                }
+
+                if (window.postman) {
+                    window.postman.sendMessage(data);
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    } else {
+        console.log('La contraseña no es válida');
+    }
 }
