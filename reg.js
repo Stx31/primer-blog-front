48609ced64272402be5e5b4167a3b9c10b5c0267
Stx31 <kh1327303@gmail.com>
2023-12-12 15:53:23 +0100

    hora

--- reg.js
+++ reg.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     loadMessages();
+    // Agregado: Llama a la función loadAuthors al cargar la página
+    loadAuthors();
 });
 
 function savePost() {
@@ -7,20 +9,41 @@ function savePost() {
     const title = document.getElementById('title').value;
     const message = document.getElementById('message').value;
 
+    const currentDate = new Date();
+    const formattedDateTime = `${getDayName(currentDate)} ${currentDate.getDate()} de ${getMonthName(currentDate)} a las ${formatTime(currentDate)}`;
+
+    // Agregado: Actualiza el elemento en el HTML con la fecha y la hora actual
+    updateCurrentDateTime(formattedDateTime);
+
     fetch('http://localhost:3000/guardarPost', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ author, title, message }),
+        body: JSON.stringify({ author, title, message, dateTime: formattedDateTime }),
     })
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        
         window.location.href = 'index.html';
     })
     .catch(error => handleError(error, 'Error al guardar el mensaje'));
+}
+
+function getDayName(date) {
+    const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    return days[date.getDay()];
+}
+
+function getMonthName(date) {
+    const months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+    return months[date.getMonth()];
+}
+
+function formatTime(date) {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
 }
 
 function loadMessages() {
@@ -35,25 +58,25 @@ function loadMessages() {
             const messagesContainer = document.getElementById('messagesContainer');
             messagesContainer.innerHTML = '';
 
-            data.posts.forEach(({ author, title, message }) => {
-                const messageDiv = createMessageDiv(title, author, message);
+            data.posts.forEach(({ author, title, message, dateTime }) => {
+                const messageDiv = createMessageDiv(title, author, message, dateTime);
                 messagesContainer.appendChild(messageDiv);
             });
         })
         .catch(error => handleError(error, 'Error al cargar los mensajes'));
 }
 
-function createMessageDiv(title, author, message) {
+function createMessageDiv(title, author, message, dateTime) {
     const messageDiv = document.createElement('div');
     messageDiv.classList.add('message');
-    messageDiv.innerHTML = `<h4>${title}</h4><p>Autor: ${author}</p><p>${message}</p>`;
+    messageDiv.innerHTML = `<h4>${title}</h4><p>Autor: ${author}</p><p>${message}</p><p>${dateTime}</p>`;
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Borrar';
     deleteButton.classList.add('delete-button');
     deleteButton.addEventListener('click', function () {
-        deleteMessage(author, title, message); 
-        messageDiv.remove(); 
+        deleteMessage(author, title, message);
+        messageDiv.remove();
     });
 
     messageDiv.appendChild(deleteButton);
@@ -75,7 +98,6 @@ function deleteMessage(author, title, message) {
     .catch(error => handleError(error, 'Error al borrar el mensaje'));
 }
 
-
 function loadAuthors() {
     fetch('http://localhost:3000/obtenerPosts')
         .then(response => response.json())
@@ -87,15 +109,16 @@ function loadAuthors() {
 
             data.posts.forEach(post => {
                 const author = post.author;
-                if (!authorsArray.includes(author)) {
-                    authorsArray.push(author);
+                const dateTime = post.dateTime; // Agregado: Obtener la fecha y la hora del mensaje
+                if (!authorsArray.some(item => item.author === author)) {
+                    authorsArray.push({ author, dateTime });
 
                     const authorBox = document.createElement('div');
                     authorBox.classList.add('author-box');
 
                     const authorItem = document.createElement('span');
                     authorItem.classList.add('author-name');
-                    authorItem.textContent = author;
+                    authorItem.textContent = `${author} - ${dateTime}`; // Modificado: Mostrar la fecha y la hora
 
                     const deleteButton = document.createElement('button');
                     deleteButton.textContent = 'Borrar';
@@ -113,6 +136,7 @@ function loadAuthors() {
         })
         .catch(error => handleError(error, 'Error al cargar los autores'));
 }
+
 function deleteAuthor(author) {
     fetch('http://localhost:3000/eliminarAutor', {
         method: 'DELETE',
@@ -124,7 +148,15 @@ function deleteAuthor(author) {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        loadAuthors();  
+        loadAuthors();
     })
     .catch(error => handleError(error, 'Error al borrar el autor'));
+}
+
+// Agregado: Función para actualizar el elemento en el HTML con la fecha y la hora actual
+function updateCurrentDateTime(formattedDateTime) {
+    const currentDateTimeElement = document.getElementById('currentDateTime');
+    if (currentDateTimeElement) {
+        currentDateTimeElement.textContent = formattedDateTime;
+    }
 }
